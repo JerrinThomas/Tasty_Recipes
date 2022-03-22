1a53ece67b00ee0840c74f994e0da45c130839a1
suelen-vicente <suelen.blasques@gmail.com>
2022-03-22 12:11:06 -0400

    Creating Meal, Category and Ingredient Models

---
+++ TastyRecipes/TastyRecipes/Models/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  TastyRecipes
+//
+//  Created by Suelen Vicente on 2022-03-22.
+//
+
+import Foundation
+import UIKit
+
+struct Category{
+    var idCategory: Int
+    var strCategory: String
+    var strCategoryThumb: UIImage
+    var strCategoryDescription: String
+}
---
+++ TastyRecipes/TastyRecipes/Models/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredient.swift
+//  TastyRecipes
+//
+//  Created by Suelen Vicente on 2022-03-22.
+//
+
+import Foundation
+
+struct Ingredient{
+    var idIngredient: Int
+    var strIngredient: String
+    var measure: String
+}
