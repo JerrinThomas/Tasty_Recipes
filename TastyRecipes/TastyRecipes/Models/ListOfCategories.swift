09758399b5160d1b5f2e50a9907aa561edd422fc
suelen-vicente <suelen.blasques@gmail.com>
2022-03-25 23:06:19 -0400

    Sue - Refactor

---
+++ TastyRecipes/TastyRecipes/Models/ListOfCategories.swift
@@ -0,0 +1,8 @@
+//
+//  ListOfCategories.swift
+//  TastyRecipes
+//
+//  Created by Suelen Vicente on 2022-03-25.
+//
+
+import Foundation
---
+++ TastyRecipes/TastyRecipes/Models/ListOfSimpleMeals.swift
@@ -0,0 +1,12 @@
+//
+//  ListOfResumedMeals.swift
+//  TastyRecipes
+//
+//  Created by Suelen Vicente on 2022-03-25.
+//
+
+import Foundation
+
+struct ListOfSimpleMeals{
+    public let meals:[SimpleMeal]
+}
