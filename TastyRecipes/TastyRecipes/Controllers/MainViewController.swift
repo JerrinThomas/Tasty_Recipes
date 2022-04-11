2ca11a13188fb7f52859d9a61aeb40e66b39cd4b
suelen-vicente <suelen.blasques@gmail.com>
2022-04-11 09:51:03 -0400

    Sue - Solving conflicts

--- TastyRecipes/TastyRecipes/Controllers/MainViewController.swift
+++ TastyRecipes/TastyRecipes/Controllers/MainViewController.swift
@@ -12,24 +12,24 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "home"), tag: 0)
+
         let searchVC = UINavigationController(rootViewController: SearchViewController())
-        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(named: "search"), tag: 0)
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(named: "search"), tag: 1)
         
         let favoritesVC = UINavigationController(rootViewController: FavoritesViewController())
-        favoritesVC.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "starIcon"), tag: 1)
+        favoritesVC.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "starIcon"), tag: 2)
         
         let accountVC = UINavigationController(rootViewController: AccountViewController())
-        accountVC.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "person"), tag: 2)
+        accountVC.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "person"), tag: 3)
         
-        viewControllers = [searchVC, favoritesVC, accountVC]
+        viewControllers = [homeVC, searchVC, favoritesVC, accountVC]
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-       
-        
-<<<<<<< Updated upstream
+
         if let _ = Auth.auth().currentUser {
         } else{
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -38,22 +38,7 @@ class MainViewController: UITabBarController {
             present(loginVC, animated: false, completion: nil)
 
         }
-=======
-        let favoritesVC = UINavigationController(rootViewController: FavoritesViewController())
-        favoritesVC.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "starIcon"), tag: 1)
-        
-        let homeVC = UINavigationController(rootViewController: HomeViewController())
-        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "home"), tag: 2)
-        
-        viewControllers = [homeVC, searchVC, favoritesVC]
-        //print("hi")
->>>>>>> Stashed changes
         
     }
-    
-    
-    
-  
-
 }
 
